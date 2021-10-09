abc5ec4ca11e971a082c5b689ede625b6b471cad
wj <jennifer@secret.chat>
2021-10-09 18:58:54 +0800

    no message

---
+++ Protos/WireProtos.h
@@ -0,0 +1,17 @@
+// 
+// 
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Protos.
+FOUNDATION_EXPORT double ProtosVersionNumber;
+
+//! Project version string for Protos.
+FOUNDATION_EXPORT const unsigned char ProtosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Protos/PublicHeader.h>
+
+#import <WireProtos/Messages.pb.h>
+#import <WireProtos/Otr.pb.h>
+
---
+++ ProtosTests/ProtosTests.swift
@@ -0,0 +1,12 @@
+
+
+import XCTest
+import WireProtos
+
+class ProtosTests: XCTestCase {
+
+    func testThatItCompiles() {
+        // We need at least one test or the build machine will be unhappy
+    }
+
+}
